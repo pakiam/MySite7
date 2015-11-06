5a84db383357ca64d6e48891d35e499d8d0c2426
Maksim <akiam@mail.ru>
2015-11-07 00:56:13 +0300

    First of all canvas isnt mine.

---
+++ www/js/main.js
@@ -0,0 +1,58 @@
+$(document).ready(function () {
+
+    var vH = $(window).height();
+    var vW = $(window).width();
+
+    // Header Scroll
+    $(window).on('scroll', function () {
+        var scroll = $(window).scrollTop();
+
+        if (scroll >= 50) {
+            $('#header').addClass('fixed');
+        } else {
+            $('#header').removeClass('fixed');
+        }
+    });
+
+    // contact form
+    $("#contactsform").submit(function () {
+        var a = $(this).attr("action");
+        $("#message").slideUp(750, function () {
+            $("#message").hide();
+            $("#submit-contacts").attr("disabled", "disabled");
+            $.post(a, {
+                name: $("#contacts-form-name").val(),
+                email: $("#contacts-form-email").val(),
+                phone: $("#contacts-form-phone").val(),
+                comments: $("#contacts-form-message").val()
+            }, function (a) {
+                document.getElementById("message").innerHTML = a;
+                $("#message").slideDown("slow");
+                $("#submit-contacts").removeAttr("disabled");
+                if (null != a.match("success")) $("#contactsform").slideDown("slow");
+            });
+        });
+        return false;
+    });
+    $("#contactsform input, #contactsform textarea").keyup(function () {
+        $("#message").slideUp(1500);
+    });
+
+    //menu scrolling
+    $(".scroll").click(function (event) {
+        event.preventDefault();
+        $('html,body').animate({scrollTop: $(this.hash).offset().top - 64}, 1000);
+    });
+
+
+    //showing items
+    $('.interactive').on('click', function () {
+        $('.items').find('.item').each(function (index, item) {
+            setTimeout(function () {
+                $(item).fadeIn('normal').css('margin-top', '50px');
+            }, 200 + (index * 500))
+        });
+        return false;
+    });
+});
+
